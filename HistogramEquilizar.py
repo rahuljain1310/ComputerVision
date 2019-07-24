e3007c9966ee3af5f36c0b38eaa90552b0e6ba35
rahuljain1310 <ee1170476@iitd.ac.in>
2019-07-24 13:47:44 +0530

    HistogramEquilize Complete

--- HistogramEquilizar.py
+++ HistogramEquilizar.py
@@ -10,38 +10,38 @@ def getIntensityCount(np_img):
       KCount[K]+=1
   return KCount
 
-def getCummulativeIntensity(np_count):
+def getCumm_Intensity(np_count):
   Cumm = numpy.zeros(256)
   Cumm[0] = np_count[0]
   for i in range(1,256):
     Cumm[i] = Cumm[i-1]+np_count[i]    
   return Cumm  
 
+def HistogramEquilize(np_img):
+  w,h = np_img.shape()
+  norm_Intensity =  getIntensityCount(np_img)/(w*h)
+  Cumm_norm_Intensity = getCumm_Intensity(norm_Intensity)
+  ## TransIntensity :- T(K) 
+  ScaleK = lambda t: int(floor(255*t)) 
+  Trans_Intensity = numpy.vectorize(ScaleK)(Cumm_norm_Intensity)
+  transformK = lambda t: Trans_Intensity[t]
+  np_equiImg = numpy.vectorize(transformK)(np_img)
+  return Cumm_norm_Intensity,np_equiImg
+
 im = Image.open('lenna.png','r')
 im_grey = im.convert('L')
 np_im_grey = numpy.array(im_grey)
 w,h = np_im_grey.shape
-
-norm_Intensity = getIntensityCount(np_im_grey)/(w*h)
-Cumm_norm_Intensity = getCummulativeIntensity(norm_Intensity)
-
-ScaleK = lambda t: int(floor(255*t))
-Trans_Intensity = numpy.vectorize(ScaleK)(Cumm_norm_Intensity)
-
-transformK = lambda t: Trans_Intensity[t]
-np_equiImg = numpy.vectorize(transformK)(np_im_grey)
-
-equi_im = Image.fromarray(np_equiImg)
+Cumm_norm_Intensity, np_equiImg = HistogramEquilize(np_im_grey)
 
 ## Show Both Images
-concat_np = numpy.concatenate((np_im_grey,np_equiImg))
-concat_img = Image.fromarray(concat_np)
+concat_np_img = numpy.concatenate((np_im_grey,np_equiImg))
+concat_img = Image.fromarray(concat_np_img)
 concat_img.show()
 
 ## Plot Cummulative Frequency Histograms
 Trans_NormIntensity = getIntensityCount(np_equiImg)/(w*h)
-Trans_CommIntensity = getCummulativeIntensity(Trans_NormIntensity)
-
+Trans_CommIntensity = getCumm_Intensity(Trans_NormIntensity)
 plt.plot(Cumm_norm_Intensity)
 plt.plot(Trans_CommIntensity)
 plt.show()
